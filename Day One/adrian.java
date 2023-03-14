9298b5f2cc9cb8c0f7df2f1e18a4f4e3d53cd779
George Adrian <103585248+georgeadrila@users.noreply.github.com>
2023-03-14 15:41:34 +0300

    Day one of java
    creating classes

---
+++ Day One/adrian.java
@@ -0,0 +1,9 @@
+public class adrian {
+    public static void main(String[] args) {
+        // create loop to print "hello george" 10 times
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello George!");
+            System.out.println(i +1);
+        }
+    }
+}
---
+++ Day One/hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello George!");
+        System.out.println("Welcome to Java programming");
+    }
+}
